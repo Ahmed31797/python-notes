c6a3f01e1bc76ad99ca285e55130c059f21aff5f
ahmed <ahmed.noor10210@gmail.com>
2024-01-26 06:53:57 -0800

    python tutorial

---
+++ operators.py
@@ -0,0 +1,26 @@
+""" 
+== - equals to
+!= - not equals to
+> - greater than
+>= - greater than or equal to
+< - less than
+<= - less than or equal to
+"""
+num1 = 90
+num2 = 90
+if num1 == num2:
+    print("Same")
+else:
+    print("Not same")
+
+# and operator
+isFeesCleared = False
+grade = "B"
+if isFeesCleared and grade == "A":
+    print("You are going to the trip")
+else:
+    print("Sorry, You are not going to the trip")
+
+
+
+
